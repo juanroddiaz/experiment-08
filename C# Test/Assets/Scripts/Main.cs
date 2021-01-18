1ce88cd9502126d4ca71b9dc3ba566bdd9946246
Juan Diaz <juan.rod.diaz@gmail.com>
2021-01-18 22:41:55 +0100

    Creating basic test framework for questions. First question resolved.

---
+++ C# Test/Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        // question 1
+        var question1 = new AllDigitsUniqueLogic();
+        if (!question1.TestAllDigitsUnique())
+        {
+            OnTestNotPassed(1);
+            return;
+        }
+
+        Debug.Log("Success!");
+    }
+
+
+    private void OnTestNotPassed(int testIndex)
+    { 
+        Debug.LogError("testIndex: " + testIndex + " NOT PASSED!");
+    }
+}
---
+++ C# Test/Assets/Scripts/Questions/AllDigitsUniqueLogic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllDigitsUniqueLogic
+{
+    private List<int> _uniqueNumbers = new List<int>();
+
+    //  Write a method that takes an unsigned integer as input and returns true if all the digits in the base
+    //  10 representation of that number are unique.
+    private bool AllDigitsUnique(int value)
+    {
+        if (value < 0)
+        {
+            return false;
+        }
+
+        _uniqueNumbers.Clear();
+        int temp = 0;
+        while (value > 0)
+        {
+            temp = value % 10;
+            if (_uniqueNumbers.Contains(temp))
+            {
+                return false;
+            }
+
+            _uniqueNumbers.Add(temp);
+            value /= 10;
+        }
+        
+        return true;
+    }
+
+    public bool TestAllDigitsUnique()
+    {
+        bool result = true;
+        result &= AllDigitsUnique(48778584) == false;
+        result &= AllDigitsUnique(17308459) == true;
+        result &= AllDigitsUnique(-1) == false;
+        result &= AllDigitsUnique(0) == true;
+        result &= AllDigitsUnique(1234567890) == true;
+        result &= AllDigitsUnique(0987654321) == true;
+        result &= AllDigitsUnique(11) == false;
+        result &= AllDigitsUnique(1) == true;
+        return result;
+    }
+}
